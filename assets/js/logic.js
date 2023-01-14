bec9fb3fa1bfa48774d1bb860b82f8605c1fb2ba
dodzikojo <dodzi@windowslive.com>
2023-01-14 05:35:51 +0000

    Functions to randomized questions and answers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,31 +8,69 @@ class questionAnswers {
 
 let getTimerEl = document.getElementById('time')
 let startQuizBtnEl = document.getElementById('start')
+let questionTextEl = document.getElementById('question-title')
+let choicesListEl = document.getElementById('choices')
+
+let questionScreenDivEl = document.getElementById('questions')
+let endScreenDivEl = document.getElementById('end-screen')
+
+document.addEventListener('click', countdown)
 
 let newQuestionAnswer = new questionAnswers()
 
-for (let index = 0; index < allQuestions_Answers.length; index++) {
-    let question = allQuestions_Answers[index][0];
-    let answers = allQuestions_Answers[index][1];
 
-    console.log(question);
+function randomizeArray (arr) {
+  var randomArr = []
 
-    for (var [key, value] of answers.entries()) {
-        console.log(key + " = " + value);
-      }
+  for (let index = 0; index < arr.length; index++) {
+    randomArr.push(arr[index])
+  }
 
-      console.log("================")
+  return randomArr.sort(function (a, b) {
+    return 0.5 - Math.random()
+  })
+}
+
+function randomizeArrayMap (map) {
+  var randomArrMap = []
+
+  for (var [key, value] of map.entries()) {
+
+    var sample = [key, value]
+    randomArrMap.push(sample)
+  }
+
+  return randomArrMap.sort(function (b, a) {
+    return 0.5 - Math.random()
+  })
+}
+
+let randomizedQuestionsArray = randomizeArray(allQuestions_Answers)
+
+for (let index = 0; index < randomizedQuestionsArray.length; index++) {
+  let question = randomizedQuestionsArray[index][0]
+  let answers = randomizedQuestionsArray[index][1]
+
+  console.log('--------QUESTION--------')
+  console.log(question)
+  console.log('-------RANDOMISED ANSWERS---------')
+  let randomizedAnswers = randomizeArrayMap(answers)
+
+  for (var [key, value] of randomizedAnswers.entries()) {
+    console.log(value[0] + " = " + value[1]);
+    
+  }
+
+  console.log('================')
 }
 
 function countdown () {
-  var timeLeft = 100
+  var timeLeft = 5
 
-  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
-    //
     // YOUR CODE HERE
     getTimerEl.textContent = timeLeft--
-    if (timeLeft === -1) {
+    if (timeLeft == -1) {
       getTimerEl.textContent = '0'
       clearInterval(timeInterval)
       // displayMessage();
@@ -42,4 +80,3 @@ function countdown () {
   }, 1000)
 }
 
-document.addEventListener('click', countdown)
