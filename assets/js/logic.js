54c09c3d3aed3d5f7f633f825d61c966e75ca966
dodzikojo <dodzi@windowslive.com>
2023-01-15 13:15:15 +0000

    Added the ability to show the questions asked after quiz completes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,29 @@
 import { allQuestions_Answers } from '../js/questions.js'
 
 class questionAnswers {
-  constructor (questionID, questionOrder, question, multipleChoices_Answer) {
-    ;(questionID, questionID),
-      (questionOrder = questionOrder),
-      (question = question),
-      (multipleChoices_Answer = multipleChoices_Answer)
+  constructor (
+    questionNumber,
+    questionTitle,
+    multipleChoices_Answer,
+    isUserAnswerCorrect,
+    userAnswer,
+    correctAnswer,
+    explanation
+  ) {
+    ;(questionNumber = questionNumber),
+      (questionTitle = questionTitle),
+      (multipleChoices_Answer = multipleChoices_Answer),
+      (isUserAnswerCorrect = isUserAnswerCorrect),
+      (userAnswer = userAnswer),
+      (correctAnswer = correctAnswer),
+      (explanation = explanation)
   }
 }
 
 let quizTimer = 100
 let randomizedQuestionsArray
 let questionsCounter = 0
-// let answeredQuestionsID = []
+let answeredQuestions = []
 
 let getTimerEl = document.getElementById('time')
 let answerBtnEl
@@ -26,6 +37,8 @@ let startScreenDivEl = document.getElementById('start-screen')
 let questionScreenDivEl = document.getElementById('questions')
 let displayResultDivEl = document.getElementById('display-result')
 
+let expanderDivEl = document.getElementById('result-expander')
+
 let clockTickAudioEl = document.getElementById('clock-ticker')
 // let clockTickAudioEl = document.createElement('audio')
 // clockTickAudioEl.setAttribute("type","audio/mpeg")
@@ -34,7 +47,6 @@ let clockTickAudioEl = document.getElementById('clock-ticker')
 
 // questionScreenDivEl.appendChild(clockTickAudioEl);
 // clockTickAudioEl.play();
-
 
 var submitBtnEl = document.querySelector('#submit')
 
@@ -98,21 +110,117 @@ function randomizeArrayMap (map) {
   })
 }
 
+function createQuestion_AnswerCards (
+  questionNumber,
+  questionTitle,
+  userAnswer,
+  isUserAnswerCorrect,
+  correctAnswer,
+  explanation
+) {
+  let mainCard = document.createElement('div')
+  mainCard.setAttribute('class', 'card text-dark bg-light mt-3 mb-3')
+
+  let cardHeader = document.createElement('div')
+  cardHeader.setAttribute('class', 'card-header')
+
+  let cardTitleEl_QuestionAnswer = document.createElement('h5')
+  cardTitleEl_QuestionAnswer.setAttribute(
+    'class',
+    'card-title left-align mt-1 mb-1 fw-bold'
+  )
+  cardTitleEl_QuestionAnswer.setAttribute('style', 'font-size: 16px;')
+  cardTitleEl_QuestionAnswer.textContent = questionNumber + '. ' + questionTitle
+
+  let cardTitleSpanEl_CurrentAnswer = document.createElement('span')
+  cardTitleSpanEl_CurrentAnswer.setAttribute('style', 'font-weight: bold')
+
+  let cardBody = document.createElement('div')
+  cardBody.setAttribute('class', 'card-body')
+
+  let imageEl = document.createElement('img')
+  imageEl.setAttribute('class', 'd-inline me-2')
+  imageEl.setAttribute('style', 'height: 40px')
+
+  let answer_ExplanationLabelEl = document.createElement('h5')
+  answer_ExplanationLabelEl.setAttribute('class', 'card-text')
+  answer_ExplanationLabelEl.style.fontSize = '14px'
+
+  let correctAnswer_El = document.createElement('h4')
+
+  correctAnswer_El.style.fontSize = '17px'
+  correctAnswer_El.textContent = correctAnswer
+
+  let cardTextEl_UserAnswer = document.createElement('h5')
+  cardTextEl_UserAnswer.style.fontSize = '16px'
+  cardTextEl_UserAnswer.textContent = userAnswer
+
+  if (isUserAnswerCorrect == true) {
+    imageEl.setAttribute('src', '././assets/images/correct.png')
+    answer_ExplanationLabelEl.textContent = 'Explanation'
+    correctAnswer_El.setAttribute(
+      'class',
+      'card-text left-align fw-bold d-none'
+    )
+    cardTextEl_UserAnswer.setAttribute(
+      'class',
+      'card-title left-align d-inline fw-bold'
+    )
+  } else {
+    imageEl.setAttribute('src', '././assets/images/incorrect.png')
+    answer_ExplanationLabelEl.textContent = 'Correct Answer and Explanation'
+    correctAnswer_El.setAttribute('class', 'card-text left-align fw-bold ')
+    cardTextEl_UserAnswer.setAttribute(
+      'class',
+      'card-title left-align d-inline'
+    )
+  }
+
+  let hrEl_line = document.createElement('hr')
+  hrEl_line.setAttribute('class', 'my-2')
+
+  let cardText_Explanation = document.createElement('p')
+  cardText_Explanation.setAttribute('class', 'card-text left-align')
+  cardText_Explanation.style.fontSize = '15px'
+  cardText_Explanation.style.fontStyle = 'italic'
+  cardText_Explanation.textContent = explanation
+
+  cardBody.appendChild(imageEl)
+  cardBody.appendChild(cardTextEl_UserAnswer)
+  cardBody.appendChild(hrEl_line)
+  cardBody.appendChild(answer_ExplanationLabelEl)
+  cardBody.appendChild(correctAnswer_El)
+
+  cardBody.appendChild(cardText_Explanation)
+
+  cardHeader.appendChild(cardTitleEl_QuestionAnswer)
+  mainCard.appendChild(cardHeader)
+  mainCard.appendChild(cardBody)
+
+  expanderDivEl.appendChild(mainCard)
+}
+
 function setSingleQuestion_Answers (randomizedQuestionsArray, questionsCounter) {
   if (questionsCounter >= allQuestions_Answers.length) {
   } else {
     let newQuestionAnswer = new questionAnswers()
-    newQuestionAnswer.questionOrder = questionsCounter
-    newQuestionAnswer.question = randomizedQuestionsArray[questionsCounter][0] //Gets the Question
+    newQuestionAnswer.questionNumber = questionsCounter
+    newQuestionAnswer.questionTitle =
+      randomizedQuestionsArray[questionsCounter][0] //Gets the Question
+
     newQuestionAnswer.multipleChoices_Answer =
       randomizedQuestionsArray[questionsCounter][1] //Gets the Answers
+    newQuestionAnswer.explanation =
+      randomizedQuestionsArray[questionsCounter][2]
 
     let randomizedAnswers = randomizeArrayMap(
       newQuestionAnswer.multipleChoices_Answer
     )
     choicesListEl = document.getElementById('choices')
 
-    questionTextEl.textContent = newQuestionAnswer.question
+    // console.log("here you go: "+newQuestionAnswer.multipleChoices_Answer);
+
+    questionTextEl.textContent = newQuestionAnswer.questionTitle
 
     let answerCounter = 1
     let correntAnswerIndex = 0
@@ -128,6 +236,7 @@ function setSingleQuestion_Answers (randomizedQuestionsArray, questionsCounter) 
       //Get Correct Answer Index
       if (value[1] === true) {
         correntAnswerIndex = answerIndex
+        newQuestionAnswer.correctAnswer = value[0]
       }
 
       answerBtnEl.setAttribute('response-index', answerIndex)
@@ -139,12 +248,24 @@ function setSingleQuestion_Answers (randomizedQuestionsArray, questionsCounter) 
 
         //Check if user has submitted the correct answer
         if (userAnswerIndex == correntAnswerIndex) {
+          newQuestionAnswer.isUserAnswerCorrect = true
           resultValueTextEl.textContent = 'Correct!'
           playAudio('././assets/sfx/correct.wav')
         } else {
+          newQuestionAnswer.isUserAnswerCorrect = false
           playAudio('././assets/sfx/incorrect.wav')
           resultValueTextEl.textContent = 'Wrong! (-10 seconds)'
           quizTimer = quizTimer - 10
+        }
+
+        let indexCounter = 0
+        for (var [key, value] of randomizedAnswers.entries()) {
+          //Get user's selected answer text.
+          if (indexCounter == userAnswerIndex) {
+            newQuestionAnswer.userAnswer = value[0]
+            break
+          }
+          indexCounter++
         }
       })
 
@@ -155,6 +276,9 @@ function setSingleQuestion_Answers (randomizedQuestionsArray, questionsCounter) 
       answerBtnEl.textContent = answerCounter++ + '. ' + value[0]
       choicesListEl.appendChild(answerBtnEl)
     }
+
+    // newQuestionAnswer.correctAnswer = "ABCD";
+    answeredQuestions.push(newQuestionAnswer)
   }
 }
 
@@ -190,8 +314,8 @@ function playAudio (url) {
 }
 
 function pauseAudio (url) {
-    new Audio(url).pause()
-  }
+  new Audio(url).pause()
+}
 
 function countdown () {
   let timerDivEl = document.getElementById('timer')
@@ -211,7 +335,7 @@ function countdown () {
     getTimerEl.textContent = quizTimer--
 
     if (quizTimer < 10) {
-        clockTickAudioEl.play()
+      clockTickAudioEl.play()
     }
 
     if (quizTimer < 30) {
@@ -219,7 +343,7 @@ function countdown () {
       timerDivEl.style.fontWeight = 'bold'
     }
     if (quizTimer == 0) {
-        clockTickAudioEl.pause()
+      clockTickAudioEl.pause()
       getTimerEl.textContent = '0'
       clearInterval(timeInterval)
       questionScreenDivEl.setAttribute('class', 'hide')
@@ -233,9 +357,22 @@ function countdown () {
       questionScreenDivEl.setAttribute('class', 'hide')
       timerDivEl.setAttribute('class', 'hide')
       displayResultDivEl.setAttribute('class', 'hide')
+
+      answeredQuestions.forEach(questionAnswer => {
+        createQuestion_AnswerCards(
+          ++questionAnswer.questionNumber,
+          questionAnswer.questionTitle,
+          questionAnswer.userAnswer,
+          questionAnswer.isUserAnswerCorrect,
+          questionAnswer.correctAnswer,
+          questionAnswer.explanation
+        )
+      })
+
       endScreenDivEl.setAttribute('class', '')
 
       finalScoreTextEl.textContent = quizTimer
+      console.log(answeredQuestions)
     }
 
     //
